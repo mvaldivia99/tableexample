39d4aa5e9f33d5333626e1b858968b08e8b7131a
Valdivia <MichellV@bigbend.edu>
2025-03-05 15:13:14 -0800

    Added Row Class
    Added class Row with attributes

--- Controller.java
+++ Controller.java
@@ -1,3 +1,10 @@
 // description: this is a class that will control the flow of the program
 // author: Michell Valdivia
 // CS 141 example week 10
+
+//
+class Controller{
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Row.java
@@ -0,0 +1,21 @@
+// this class will represent a row in a table
+
+public class Row {
+    int id;
+    String prodName;
+    double price;
+
+    // set defaut row
+    Row(){
+        id = -1;
+        prodName = null;
+        price = -1.0;
+    }
+
+    // 3-arg 
+    Row(int id, String prodName, double price){
+        this.id = id;
+        this.prodName = prodName;
+        this.price = price;
+    }
+}
